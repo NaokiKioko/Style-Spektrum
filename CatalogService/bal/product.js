6530c07842bd93fb376b0299985da55825e4cac5
NaokiKioko <DamienKrock@gmail.com>
2024-10-23 12:59:08 -0600

    await

--- CatalogService/bal/product.js
+++ CatalogService/bal/product.js
@@ -1,24 +1,29 @@
 const dal = require('../dal/mongoDB.js');
 
-function GetProducts() {
-    return dal.Interface("get", "product", {});
+async function GetProducts() {
+    return await dal.Interface("get", "product", {});
 }
 
-function GetProduct(id) {
-    return dal.Interface("get", "product", {"_id":id});
+async function GetProduct(id) {
+    return await dal.Interface("get", "product", {"_id":id});
 }
 
-function PostProducts(product) {
-    return dal.Interface("post", "product", product);
+async function PostProducts(product) {
+    return await dal.Interface("post", "product", product);
 }
 
-function PatchProducts(id, product) {
-    return dal.Interface("patch", "product", [{"_id":id}, product]);
+async function PatchProducts(id, product) {
+    return await dal.Interface("patch", "product", [{"_id":id}, product]);
 }
 
-function DeleteProducts(id) {
-    return dal.Interface("delete", "product", {"_id":id});
+async function DeleteProducts(id) {
+    return await dal.Interface("delete", "product", {"_id":id});
 }
 
 module.exports = {
+    GetProducts,
+    GetProduct,
+    PostProducts,
+    PatchProducts,
+    DeleteProducts
 }
