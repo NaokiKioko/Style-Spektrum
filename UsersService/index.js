b39754e915032a8900f4ec6c083c94f2b3128456
NaokiKioko <DamienKrock@gmail.com>
2024-10-30 12:43:48 -0600

    conversion

--- UsersService/index.js
+++ UsersService/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const auth = require('./auth/auth.js');
 const bal = require('./bal/users.js');
+const DatabaseInterface = require('./dal/mongoDB.js'); // Import the class
+const dal = new DatabaseInterface();
 const port = 3000;
 
 app.use(express.json());
@@ -13,7 +15,7 @@ app.get('/', (req, res) => {
 app.post('/register', async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    const code = await bal.registerUser(email, password);
+    const code = await bal.registerUser(dal, email, password);
     if (code === 200) {
         return res.status(200).send('User registered!');  // return to prevent further execution
     } else if (code === 400) {
@@ -24,7 +26,7 @@ app.post('/register', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-    let response = await bal.login(req.body.email, req.body.password);
+    let response = await bal.login(dal, req.body.email, req.body.password);
     if (response.statuscode === 401) {
         res.status(401).send('Invalid email or password!');
     } else if (response.statuscode === 404) {
@@ -35,7 +37,7 @@ app.post('/login', async (req, res) => {
 });
 
 app.get('/me', auth.authenticateToken, async (req, res) => {
-    let user = await bal.GetUserByEmail(req.user.email);
+    let user = await bal.GetUserByEmail(dal, req.user.email);
     res.status(200).send(user);
 });
 
@@ -43,14 +45,14 @@ app.get('/logout', (req, res) => {
     res.status(200).send('just delete the jwt <3');
 });
 
-app.get("/authenticate", auth.authenticateToken, (req, res) => {
+app.get("/authenticate", auth.authenticateToken, (dal, req, res) => {
     res.send('Authenticated user: ' + req.user.email);
 });
 
 app.delete("/delete", auth.authenticateToken, async (req, res) => {
     let email = req.user.email
     let password = req.body.password
-    let code = await bal.deleteAccount(email, password)
+    let code = await bal.deleteAccount(dal, email, password)
     if (code === 200) {
         res.status(code).send('User deleted!')
     } else {
